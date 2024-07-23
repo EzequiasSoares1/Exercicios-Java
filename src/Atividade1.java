d1a339321211ad0bf3148acdde8e09b9463eb978
Ezequias Soares <87997012+EzequiasSoares1@users.noreply.github.com>
2024-07-22 21:50:49 -0300

    att1

---
+++ src/Atividade1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Atividade1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int classeA = 50;
+        int classeB = 30;
+        int classeC = 20;
+
+        System.out.print("Quantos bilhete de classe A foram vendidos: ");
+        int qtdA = entrada.nextInt() * classeA;
+        System.out.print("Quantos bilhete de classe B foram vendidos: ");
+        int qtdB = entrada.nextInt() * classeB;
+        System.out.print("Quantos bilhete de classe C foram vendidos: ");
+        int qtdC= entrada.nextInt() * classeC;
+
+        System.out.printf("foram vendidos %d da classe A, %d da classe B e %d da classe C.", qtdA , qtdB, qtdC);
+    }
+}
